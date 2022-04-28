da1f999e76262774b435ace445b77cd632b4e744
yancostasc <aidanheiro@gmail.com>
2022-04-28 04:48:24 -0300

    Retirada do mask que atrapalhava na compilação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { NgxMaskModule, IConfig } from 'ngx-mask';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, NgxMaskModule.forRoot()],
+  imports: [BrowserModule, FormsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })

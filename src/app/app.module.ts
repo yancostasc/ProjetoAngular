ca87b6ce4733345f6ac558ea9d41586faefaa157
yancostasc <aidanheiro@gmail.com>
2022-04-28 04:46:28 -0300

    Mask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, NgxMaskModule.forRoot()],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })

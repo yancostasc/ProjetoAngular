290c55a2c6b11d2c8e3b035cf605df87ecd2f2db
yancostasc <aidanheiro@gmail.com>
2022-05-26 18:08:45 -0300

    FPF-005 - Sidebar adicionada e css/html do autonomo melhorado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, SidebarComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

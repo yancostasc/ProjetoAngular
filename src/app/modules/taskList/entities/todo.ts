0a842ae9949c85c7e104aec820fcd376a6f49482
yancostasc <aidanheiro@gmail.com>
2022-05-31 06:12:08 -0300

    Tasklist melhorada

--- src/app/modules/taskList/entities/todo.ts
+++ src/app/modules/taskList/entities/todo.ts
@@ -1,3 +1,8 @@
 export class Todo {
-  constructor(public title: string, public description: string) {}
+  constructor(
+    public title: string,
+    public description: string,
+    public done: boolean,
+    public edit: boolean
+  ) {}
 }
--- src/app/modules/taskList/view/task-list/task-list.component.ts
+++ src/app/modules/taskList/view/task-list/task-list.component.ts
@@ -10,6 +10,7 @@ import { Todo } from '../../../taskList/entities/todo';
 export class TaskListComponent implements OnInit {
   public tasks: Todo[] = [];
   public form: FormGroup;
+  public isCreating: boolean;
 
   constructor(private formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
@@ -26,10 +27,20 @@ export class TaskListComponent implements OnInit {
     this.tasks = JSON.parse(storagedTasks);
   }
 
+  showForm(): boolean {
+    if (this.isCreating === true) {
+      return (this.isCreating = false);
+    }
+    return (this.isCreating = true);
+  }
+
   add() {
     const title = this.form.controls['title'].value;
     const description = this.form.controls['description'].value;
-    this.tasks.push(new Todo(title, description));
+    const done = false;
+    const edit = false;
+    this.tasks.push(new Todo(title, description, done, edit));
+    console.log(localStorage.getItem('tasks'));
     this.localSave();
     this.clear();
   }
@@ -47,5 +58,31 @@ export class TaskListComponent implements OnInit {
     const index = this.tasks.indexOf(todo);
     this.tasks.splice(index, 1);
     this.localSave();
+  }
+
+  markAsDone(todo: Todo) {
+    if (todo.done === true) {
+      todo.done = false;
+    } else {
+      todo.done = true;
+    }
+    this.localSave();
+  }
+
+  markAsEdit(todo: Todo) {
+    if (todo.edit === true) {
+      return (todo.edit = false);
+    }
+    return (todo.edit = true);
+  }
+
+  changeFn(value, todo: Todo, isTitle: boolean) {
+    const newValue = value.target.value;
+
+    if (isTitle === true) {
+      todo.title = newValue;
+    } else {
+      todo.description = newValue;
+    }
   }
 }
